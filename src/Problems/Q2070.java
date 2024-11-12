1994d5467f9e4d719d5561c2b914355dfe912d1b
Artem Balan <a.s.balan96@gmail.com>
2024-11-12 18:44:22 +0200

    2070 problem 2 try

--- src/Problems/Q2070.java
+++ src/Problems/Q2070.java
@@ -2,32 +2,29 @@ package Problems;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.TreeMap;
 
 public class Q2070 {
     public int[] maximumBeauty(int[][] items, int[] queries) {
 
         Arrays.sort(items, Comparator.comparingInt(a -> a[0]));
-        int maxPrice = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(0, 0);
+
         for(int[] i: items) {
-            if(i[0] > maxPrice) maxPrice = i[0];
-        }
-        int[] dp = new int[maxPrice + 1];
-        int prevMax = 0;
-        int prevIndex = 0;
-        for(int[] i: items) {
-            if(i[0] > prevIndex) {
-                Arrays.fill(dp, prevIndex, i[0], prevMax);
+            if(map.containsKey(i[0])) {
+                map.put(i[0], Math.max(i[1], map.get(i[0])));
+            } else {
+                map.put(i[0], Math.max(i[1], map.lowerEntry(i[0]).getValue()));
             }
-            dp[i[0]] = Math.max(prevMax, i[1]);
-            prevMax = dp[i[0]];
-            prevIndex = i[0];
         }
         for(int i = 0; i < queries.length; i++) {
-            int n = queries[i];
-            if(n > maxPrice) {
-                n = maxPrice;
+            if(map.containsKey(queries[i])) {
+                queries[i] = map.get(queries[i]);
+            } else {
+                queries[i] = map.lowerEntry(queries[i]).getValue();
             }
-            queries[i] = dp[n];
+
         }
 
         return queries;
