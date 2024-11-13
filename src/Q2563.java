f8c384611667ff7d9e45d46a5dbfa4ba75e3dac9
Artem Balan <a.s.balan96@gmail.com>
2024-11-13 18:00:25 +0200

    2563 problem

--- src/Q2563.java
+++ src/Q2563.java
@@ -1,13 +1,29 @@
+import java.util.Arrays;
+
 public class Q2563 {
     public long countFairPairs(int[] nums, int lower, int upper) {
-        int count = 0;
-        for(int i = 0; i < nums.length - 1; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] >= lower && nums[i] + nums[j] <= upper) {
-                    count++;
-                }
-            }
+        Arrays.sort(nums);
+        long count = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            int leftBoundaryIndex = binarySearch(nums, lower - nums[i], i + 1);
+            int rightBoundaryIndex = binarySearch(nums, upper - nums[i] + 1, i + 1);
+
+            count += rightBoundaryIndex - leftBoundaryIndex;
         }
         return count;
+    }
+
+    private int binarySearch(int[] nums, int target, int startIdx) {
+        int endIdx = nums.length;
+        while (startIdx < endIdx) {
+            int midIdx = (startIdx + endIdx) >> 1;
+            if (nums[midIdx] >= target) {
+                endIdx = midIdx;
+            } else {
+                startIdx = midIdx + 1;
+            }
+        }
+        return startIdx;
     }
 }
