4944a3ac44dba43311728e57b90d2b2ecf8eb118
Artem Balan <a.s.balan96@gmail.com>
2024-12-01 11:52:11 +0200

    1346 problem

---
+++ src/Problems/Q1346.java
@@ -0,0 +1,16 @@
+package Problems;
+
+import java.util.HashSet;
+
+public class Q1346 {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            if (set.contains(i * 2)) return true;
+            if (i % 2 == 0 && set.contains(i / 2)) return true;
+            set.add(i);
+        }
+
+        return false;
+    }
+}
