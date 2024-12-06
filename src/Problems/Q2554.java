2c5437a53e8d1ba3719f68c9cba90dcf0ccb1eeb
Artem Balan <a.s.balan96@gmail.com>
2024-12-06 16:00:51 +0200

    2554 problem fixed length without if's

--- src/Problems/Q2554.java
+++ src/Problems/Q2554.java
@@ -2,15 +2,13 @@ package Problems;
 
 public class Q2554 {
     public int maxCount(int[] banned, int n, int maxSum) {
-        boolean[] nums = new boolean[n + 1];
+        boolean[] nums = new boolean[10001];
         for(int i: banned) {
-            if(i <= n) {
-                nums[i] = true;
-            }
+            nums[i] = true;
         }
         int sum = 0;
         int count = 0;
-        for(int i = 1; i < nums.length && i <= maxSum; i++) {
+        for(int i = 1; i <= n && i <= maxSum; i++) {
             if(sum + i > maxSum) {
                 break;
             }
@@ -21,5 +19,4 @@ public class Q2554 {
         }
         return count;
     }
-
 }
