84ae3b8883a7436373b05b2f25b0a4b0b709a308
Artem Balan <a.s.balan96@gmail.com>
2024-12-06 15:59:50 +0200

    2554 problem using boolean array instead of int

--- src/Problems/Q2554.java
+++ src/Problems/Q2554.java
@@ -2,23 +2,24 @@ package Problems;
 
 public class Q2554 {
     public int maxCount(int[] banned, int n, int maxSum) {
-        int[] nums = new int[n + 1];
+        boolean[] nums = new boolean[n + 1];
         for(int i: banned) {
             if(i <= n) {
-                nums[i] = 1;
+                nums[i] = true;
             }
         }
         int sum = 0;
         int count = 0;
         for(int i = 1; i < nums.length && i <= maxSum; i++) {
-            if(nums[i] == 0) {
-                if(sum + i > maxSum) {
-                    break;
-                }
+            if(sum + i > maxSum) {
+                break;
+            }
+            if(!nums[i]) {
                 sum += i;
                 count++;
             }
         }
         return count;
     }
+
 }
