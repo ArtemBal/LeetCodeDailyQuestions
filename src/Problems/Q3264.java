53a0b560e89d9cb54d6197cd45ee96e13c21b4db
Artem Balan <a.s.balan96@gmail.com>
2024-12-16 11:10:48 +0200

    3264 problem best try

--- src/Problems/Q3264.java
+++ src/Problems/Q3264.java
@@ -3,17 +3,21 @@ package Problems;
 import java.util.PriorityQueue;
 
 public class Q3264 {
-    public int[] getFinalState(int[] nums, int k, int multiplier) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-        for(int i = 0; i < nums.length; i++) {
-            pq.offer(new int[] {nums[i], i});
+    public int[] getFinalState(int[] arr, int k, int multiplier) {
+        int[] ans = new int[arr.length];
+        while(k > 0)
+        {
+            int x = 0;
+            for(int i = 1; i < arr.length; i++)
+            {
+                if(arr[i] < arr[x])
+                {
+                    x = i;
+                }
+            }
+            arr[x] *= multiplier;
+            k--;
         }
-        while(k-- > 0) {
-            int[] arr = pq.poll();
-            arr[0] *= multiplier;
-            nums[arr[1]] = arr[0];
-            pq.offer(arr);
-        }
-        return nums;
+        return arr;
     }
 }
