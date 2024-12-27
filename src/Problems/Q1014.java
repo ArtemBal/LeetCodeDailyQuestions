979b35e4740c52bb7f9f5f38249185c9f11c6877
Artem Balan <a.s.balan96@gmail.com>
2024-12-27 18:18:41 +0200

    1014 second try (math.max slower than if's)

--- src/Problems/Q1014.java
+++ src/Problems/Q1014.java
@@ -6,8 +6,12 @@ public class Q1014 {
         int ans = 0;
         int maxPair = values[0];
         for(int j = 1; j < n; j++) {
-            ans = Math.max(ans, values[j] - j + maxPair);
-            maxPair = Math.max(maxPair, values[j] + j);
+            if(values[j] - j + maxPair > ans) {
+                ans = values[j] - j + maxPair;
+            }
+            if(values[j] + j > maxPair) {
+                maxPair = values[j] + j;
+            }
         }
         return ans;
     }
