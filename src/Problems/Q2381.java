57b58f9d3a2b694a4c1ef7a2678a256bb87adb88
Artem Balan <a.s.balan96@gmail.com>
2025-01-05 18:16:33 +0200

    2381 diff array instead of usual one

--- src/Problems/Q2381.java
+++ src/Problems/Q2381.java
@@ -2,12 +2,15 @@ package Problems;
 
 public class Q2381 {
     public String shiftingLetters(String s, int[][] shifts) {
-        int[] totalShift = new int[s.length()];
+        int[] totalShift = new int[s.length() + 1];
         for(int[] arr: shifts) {
             int dir = arr[2] == 0 ? -1 : 1;
-            for(int i = arr[0]; i <= arr[1]; i++) {
-                totalShift[i] += dir;
-            }
+            totalShift[arr[0]] += dir;
+            totalShift[arr[1] + 1] -= dir;
+        }
+
+        for(int i = 1; i < totalShift.length; i++) {
+            totalShift[i] += totalShift[i - 1];
         }
 
         char[] ans = s.toCharArray();
