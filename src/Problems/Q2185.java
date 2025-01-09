3e42d85d808443e980dc0d378a46ef80174d44c1
Artem Balan <a.s.balan96@gmail.com>
2025-01-09 11:14:32 +0200

    2185 first try

---
+++ src/Problems/Q2185.java
@@ -0,0 +1,13 @@
+package Problems;
+
+public class Q2185 {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for(String w: words) {
+            if(w.startsWith(pref)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
