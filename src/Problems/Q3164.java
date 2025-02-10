533bfef1c8a8c973f0741525b85f5b06a8d94436
Artem Balan <a.s.balan96@gmail.com>
2025-02-10 14:20:03 +0200

    3164 without deque

--- src/Problems/Q3164.java
+++ src/Problems/Q3164.java
@@ -1,22 +1,15 @@
 package Problems;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class Q3164 {
     public String clearDigits(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
         for(char c: s.toCharArray()) {
             if(Character.isDigit(c)) {
-                stack.pop();
+                sb.deleteCharAt(sb.length() - 1);
             } else {
-                stack.push(c);
+                sb.append(c);
             }
         }
-        StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
