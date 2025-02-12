ed1ac1960de6fdca0cf6e6ab0ef66178312743bb
Artem Balan <a.s.balan96@gmail.com>
2025-02-12 18:49:44 +0200

    1910

---
+++ src/Problems/Q1910.java
@@ -0,0 +1,10 @@
+package Problems;
+
+public class Q1910 {
+    public String removeOccurrences(String s, String part) {
+        while(s.contains(part)) {
+            s = s.replaceFirst(part, "");
+        }
+        return s;
+    }
+}
