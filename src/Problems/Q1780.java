9b5f52160e90700b859c0d8eb5f2dbe54f534216
Artem Balan <a.s.balan96@gmail.com>
2025-03-04 15:32:52 +0200

    1780 instead of set use int for last power 0ms

--- src/Problems/Q1780.java
+++ src/Problems/Q1780.java
@@ -5,17 +5,17 @@ import java.util.Set;
 
 public class Q1780 {
     public boolean checkPowersOfThree(int n) {
-        Set<Integer> set = new HashSet<>();
+        int lastPower = -1;
         while(n > 0) {
             int currPower = 0;
             while(Math.pow(3, currPower) <= n) {
                 currPower++;
             }
             currPower--;
-            if(set.contains(currPower)) {
+            if(lastPower == currPower) {
                 return false;
             }
-            set.add(currPower);
+            lastPower = currPower;
             n -= Math.pow(3, currPower);
             if(n == 0) {
                 return true;
