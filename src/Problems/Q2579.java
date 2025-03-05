2c2902d3d865e241099c9d79996cdad5a4cd1a02
Artem Balan <a.s.balan96@gmail.com>
2025-03-05 09:41:40 +0200

    2579 using formula instead

--- src/Problems/Q2579.java
+++ src/Problems/Q2579.java
@@ -2,18 +2,6 @@ package Problems;
 
 public class Q2579 {
     public long coloredCells(int n) {
-        return helper(n);
-    }
-
-    private long helper(int n) {
-        if(n == 1) {
-            return 1;
-        }
-
-        if(n == 2) {
-            return 5;
-        }
-
-        return helper(n - 1) + (4 + ((n - 2) * 4L));
+        return 1 + 2L * n * (n - 1);
     }
 }
