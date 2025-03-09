70451c2cf45749cf924fb8dc5ba18ed6973ff8ab
Artem Balan <a.s.balan96@gmail.com>
2025-03-09 14:08:48 +0200

    3208 using arraycopy instead of % of index 2ms

--- src/Problems/Q3208.java
+++ src/Problems/Q3208.java
@@ -3,10 +3,14 @@ package Problems;
 public class Q3208 {
     public int numberOfAlternatingGroups(int[] colors, int k) {
         int n = colors.length;
+        int[] arr = new int[n + n];
+        System.arraycopy(colors, 0, arr, 0, n);
+        System.arraycopy(colors, 0, arr, n, n);
+
         int count = 0;
         int curr = 1;
-        for(int i = 0; i < n + k - 2; i++) {
-            if(colors[i % n] != colors[(i + 1) % n]) {
+        for(int i = 1; i < n + k - 1; i++) {
+            if(arr[i] != arr[i - 1]) {
                 curr++;
             } else {
                 curr = 1;
@@ -17,4 +21,5 @@ public class Q3208 {
         }
         return count;
     }
+
 }
