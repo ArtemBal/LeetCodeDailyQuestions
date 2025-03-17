e04491ed2ead17657b7a619f4b96d6736b396b3c
Artem Balan <a.s.balan96@gmail.com>
2025-03-17 19:45:08 +0200

    2206 using int[] is faster for small amount

--- src/Problems/Q2206.java
+++ src/Problems/Q2206.java
@@ -4,10 +4,16 @@ import java.util.BitSet;
 
 public class Q2206 {
     public boolean divideArray(int[] nums) {
-        BitSet bitSet = new BitSet(501);
-        for(int i: nums) {
-            bitSet.flip(i);
+        int[] freq = new int[501];
+        for(int i = 0; i<nums.length; i++){
+            freq[nums[i]]++;
         }
-        return bitSet.cardinality() == 0;
+        for(int i = 0; i<501; i++){
+            if(freq[i] % 2 != 0){
+                return false;
+            }
+        }
+        return true;
+
     }
 }
