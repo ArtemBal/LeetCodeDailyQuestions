5cabb7e666b3e8c20dc13db0998ccb1ba25c3b8c
Artem Balan <a.s.balan96@gmail.com>
2025-03-24 15:30:31 +0200

    3169 using TreeMap 18ms

--- src/Problems/Q3169.java
+++ src/Problems/Q3169.java
@@ -1,20 +1,44 @@
 package Problems;
 
-import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Q3169 {
     public int countDays(int days, int[][] meetings) {
-        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
-        int last = 0;
-        int ans = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         for(int[] arr: meetings) {
             int start = arr[0];
             int end = arr[1];
-            if(start - 1 > last) {
-                ans += start - last - 1;
+            if(map.isEmpty()) {
+                map.put(start, end);
+                continue;
             }
-            last = Math.max(last, end);
+
+            Map.Entry<Integer, Integer> floor = map.floorEntry(start);
+            if(floor != null && floor.getValue() >= start) {
+                if(floor.getValue() >= end) {
+                    continue;
+                }
+                start = Math.min(start, floor.getKey());
+            }
+            Map.Entry<Integer, Integer> ceil = map.ceilingEntry(start + 1);
+            while(ceil != null && ceil.getKey() <= end + 1) {
+                map.remove(ceil.getKey());
+                if(ceil.getValue() >= end) {
+                    end = ceil.getValue();
+                    break;
+                }
+                ceil = map.ceilingEntry(start + 1);
+            }
+            map.put(start, end);
+            if(end == days && start == 1) {
+                return 0;
+            }
         }
-        return ans += days - last;
+        int ans = days;
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            ans -= entry.getValue() - entry.getKey() + 1;
+        }
+        return ans;
     }
 }
