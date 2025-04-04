a930d6de36a547b2d8a73386e8d2fe10b24e1ab7
Artem Balan <a.s.balan96@gmail.com>
2025-04-04 13:41:33 +0300

    2874 2ms

--- src/Problems/Q2874.java
+++ src/Problems/Q2874.java
@@ -3,25 +3,14 @@ package Problems;
 public class Q2874 {
     public long maximumTripletValue(int[] nums) {
         int n = nums.length;
-        long max = 0;
-        long temp = 0;
-        for(int i = 0; i < n - 2; i++) {
-            for(int j = i + 1; j < n - 1; j++) {
-                if(nums[i] > nums[j]) {
-                    temp = nums[i] - nums[j];
-                } else {
-                    continue;
-                }
-                for(int k = j + 1; k < n; k++) {
-
-                    long curr = temp * nums[k];
-                    if(curr > max) {
-                        max = curr;
-                    }
-                }
-            }
+        long ans = 0;
+        long maxD = 0;
+        long maxI = 0;
+        for(int i: nums) {
+            ans = Math.max(ans, maxD * i);
+            maxD = Math.max(maxD, maxI - i);
+            maxI = Math.max(maxI, i);
         }
-        return max <= 0 ? 0 : max;
+        return ans;
     }
-
 }
