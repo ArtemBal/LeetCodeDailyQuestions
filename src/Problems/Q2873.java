9905808744b9d51b996eba2e0d94ac8a991a3142
Artem Balan <a.s.balan96@gmail.com>
2025-04-04 13:40:16 +0300

    2873 first try 2ms

---
+++ src/Problems/Q2873.java
@@ -0,0 +1,26 @@
+package Problems;
+
+public class Q2873 {
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        long max = Integer.MIN_VALUE;
+        long temp = 0;
+        for(int i = 0; i < n - 2; i++) {
+            for(int j = i + 1; j < n - 1; j++) {
+                if(nums[i] > nums[j]) {
+                    temp = nums[i] - nums[j];
+                } else {
+                    continue;
+                }
+                for(int k = j + 1; k < n; k++) {
+
+                    long curr = temp * nums[k];
+                    if(curr > max) {
+                        max = curr;
+                    }
+                }
+            }
+        }
+        return max <= 0 ? 0 : max;
+    }
+}
