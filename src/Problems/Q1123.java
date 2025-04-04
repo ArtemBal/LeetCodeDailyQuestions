b928e2b2acb5215818bcb3c561390f38002f73b7
Artem Balan <a.s.balan96@gmail.com>
2025-04-04 13:38:39 +0300

    1123 simplified helper and use only max depth with ref for lca 0ms

--- src/Problems/Q1123.java
+++ src/Problems/Q1123.java
@@ -1,27 +1,27 @@
 package Problems;
 
-import Problems.Entities.Pair;
 import Problems.Entities.TreeNode;
 
 public class Q1123 {
+    TreeNode ans = null;
+    int maxD = -1;
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        return helper(root, 0).ans;
+        helper(root, 0);
+        return ans;
     }
 
-    private Pair helper(TreeNode root, int depth) {
+    private int helper(TreeNode root, int depth) {
         if(root != null) {
-            Pair leftSub = helper(root.left, depth + 1);
-            Pair rightSub = helper(root.right, depth + 1);
-            if(leftSub.depth > rightSub.depth) {
-                return leftSub;
-            } else if(leftSub.depth < rightSub.depth) {
-                return rightSub;
-            } else if(leftSub.depth != -1){
-                return new Pair(leftSub.depth, root);
-            } else {
-                return new Pair(depth, root);
+            int leftSub = helper(root.left, depth + 1);
+            int rightSub = helper(root.right, depth + 1);
+            if(leftSub == rightSub && leftSub == maxD) {
+                ans = root;
             }
+            return Math.max(leftSub, rightSub);
         }
-        return new Pair(-1, root);
+        if(depth > maxD) {
+            maxD = depth;
+        }
+        return depth;
     }
 }
