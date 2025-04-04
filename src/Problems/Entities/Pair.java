25a9585e128b1d1e2e5135e9f5be0af33fd918ac
Artem Balan <a.s.balan96@gmail.com>
2025-04-04 13:28:19 +0300

    1123 first try 0ms

---
+++ src/Problems/Entities/Pair.java
@@ -0,0 +1,11 @@
+package Problems.Entities;
+
+public class Pair {
+    public int depth;
+    public TreeNode ans;
+    public Pair() {}
+    public Pair(int depth, TreeNode ans) {
+        this.depth = depth;
+        this.ans = ans;
+    }
+}
