26d3c6b332c63756d1e295b2d24f53d43efe0ebe
Artem Balan <a.s.balan96@gmail.com>
2025-04-16 20:40:02 +0300

    1922 0ms

--- src/Problems/Q1922.java
+++ src/Problems/Q1922.java
@@ -1,14 +1,20 @@
 package Problems;
 
 public class Q1922 {
+    private static final int MOD = (int)1e9 + 7;
     public int countGoodNumbers(long n) {
-        int mod = (int)1e9 + 7;
-        long ans = 1;
-        long index = 0;
-        while(index < n) {
-            ans *= (index++ % 2 == 0 ? 5 : 4);
-            ans %= mod;
+        return (int) (helper(5, (n + 1) / 2) * helper(4, n / 2) % MOD);
+    }
+
+    private long helper(long base, long expo) {
+        long res = 1;
+        while(expo > 0) {
+            if((expo & 1) == 1) {
+                res = (res * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            expo >>= 1;
         }
-        return (int) ans;
+        return res;
     }
 }
