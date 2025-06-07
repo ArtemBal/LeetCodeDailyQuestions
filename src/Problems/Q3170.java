f26391e2daefe8cf3b0c7ccc06418c2cb79f5657
Artem Balan <a.s.balan96@gmail.com>
2025-06-07 17:01:32 +0300

    3170 1030ms

--- src/Problems/Q3170.java
+++ src/Problems/Q3170.java
@@ -3,29 +3,23 @@ package Problems;
 public class Q3170 {
     public String clearStars(String s) {
         int[] count = new int[26];
+        StringBuilder sb = new StringBuilder(s.length());
         for(char c: s.toCharArray()) {
-            int curr = c - 'a';
+
             if(c != '*') {
+                int curr = c - 'a';
+                sb.append(c);
                 count[curr]++;
             } else {
                 int smallestIndex = 0;
                 while(count[smallestIndex] == 0) {
                     smallestIndex++;
                 }
+                sb.deleteCharAt(sb.lastIndexOf((char)(smallestIndex + 'a') + ""));
                 count[smallestIndex]--;
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for(char c: s.toCharArray()) {
-            if(c == '*') {
-                continue;
-            }
-            int curr = c - 'a';
-            if(count[curr] > 0) {
-                sb.append(c);
-                count[curr]--;
             }
         }
         return sb.toString();
     }
+
 }
