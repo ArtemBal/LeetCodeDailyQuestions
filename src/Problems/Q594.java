556af35c2af565059ef9aa3db835782969b5de7b
Artem Balan <a.s.balan96@gmail.com>
2025-06-30 22:01:47 +0300

    594 16ms HashMap instead of TreeMap

--- src/Problems/Q594.java
+++ src/Problems/Q594.java
@@ -1,24 +1,21 @@
 package Problems;
 
-import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Q594 {
     public int findLHS(int[] nums) {
-        TreeMap<Integer, Integer> freq = new TreeMap<>();
+        Map<Integer, Integer> freq = new HashMap<>();
         for(int i: nums) {
             freq.merge(i, 1, Integer::sum);
         }
         int maxSubseq = 0;
-        int prevK = freq.firstKey();
-        int prevV = freq.get(prevK);
         for(var entry: freq.entrySet()) {
             int key = entry.getKey();
             int value = entry.getValue();
-            if(key - prevK == 1) {
-                maxSubseq = Math.max(maxSubseq, prevV + value);
+            if(freq.containsKey(key + 1)) {
+                maxSubseq = Math.max(maxSubseq, value + freq.get(key + 1));
             }
-            prevK = key;
-            prevV = value;
         }
         return maxSubseq;
     }
