2f20a443c4fa0836135902d6f25407a7aa0c9194
Artem Balan <a.s.balan96@gmail.com>
2025-07-10 22:33:16 +0300

    1865 157ms

---
+++ src/Problems/Q1865.java
@@ -0,0 +1,43 @@
+package Problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q1865 {
+    private int[] nums1;
+    private int[] nums2;
+    Map<Integer, Integer> map1 = new HashMap<>();
+    Map<Integer, Integer> map2 = new HashMap<>();
+
+    public Q1865(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        for(int i: nums1) {
+            map1.merge(i, 1, Integer::sum);
+        }
+        for(int i: nums2) {
+            map2.merge(i, 1, Integer::sum);
+        }
+    }
+
+    public void add(int index, int val) {
+        int curr = nums2[index];
+        if(map2.merge(curr, -1, Integer::sum) == 0) {
+            map2.remove(curr);
+        }
+        nums2[index] += val;
+        map2.merge(curr + val, 1, Integer::sum);
+    }
+
+    public int count(int tot) {
+        int ans = 0;
+        for(var m : map1.entrySet()) {
+            int k = m.getKey();
+            int v = m.getValue();
+            int temp = map2.getOrDefault(tot - k, 0);
+            ans += (v * temp);
+        }
+        return ans;
+    }
+
+}
