1f84fbb81288f9fb8f43011c2eaa393a580afaf3
Artem Balan <a.s.balan96@gmail.com>
2025-08-12 15:19:24 +0300

    2438 16ms using n & -n trick

--- src/Problems/Q2438.java
+++ src/Problems/Q2438.java
@@ -3,14 +3,12 @@ package Problems;
 public class Q2438 {
     public int[] productQueries(int n, int[][] queries) {
         final int MOD = (int)1e9 + 7;
-        String binary = Integer.toBinaryString(n);
         int[] powers = new int[Integer.bitCount(n)];
-        int index = 0;
-        for(int i = binary.length() - 1; i >= 0 ; i--) {
-            if(binary.charAt(i) == '1') {
-                powers[index] = (int)Math.pow(2, binary.length() - 1 - i);
-                index++;
-            }
+        for(int i = 0; i < powers.length; i++) {
+            int lowestSetBit = n & -n;
+            powers[i] = lowestSetBit;
+            n -= lowestSetBit;
+
         }
 
         int[] ans = new int[queries.length];
