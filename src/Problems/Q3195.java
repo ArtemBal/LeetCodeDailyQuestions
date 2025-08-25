b2cbcc38ffdb29c259dfee15ec4206eb77d98fd5
Artem Balan <a.s.balan96@gmail.com>
2025-08-25 11:21:14 +0300

    3195 1ms

--- src/Problems/Q3195.java
+++ src/Problems/Q3195.java
@@ -2,20 +2,55 @@ package Problems;
 
 public class Q3195 {
     public int minimumArea(int[][] grid) {
-        int maxX = 0;
-        int minX = 1001;
-        int maxY = 0;
-        int minY = 1001;
+        int right = 0;
+        int left = 1001;
+        int top = 0;
+        int bottom = 0;
+
+        boolean flag = false;
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] == 1) {
-                    maxX = Math.max(maxX, i);
-                    minX = Math.min(minX, i);
-                    minY = Math.min(minY, j);
-                    maxY = Math.max(maxY, j);
+                    top = i;
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag) {
+                break;
+            }
+        }
+        flag = false;
+        for(int i = grid.length - 1; i >= 0 ; i--) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1) {
+                    bottom = i;
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag) {
+                break;
+            }
+        }
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length && j < left; j++) {
+                if(grid[i][j] == 1) {
+                    left = Math.min(left, j);
+                    break;
                 }
             }
         }
-        return (maxX - minX + 1) * (maxY - minY + 1);
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = grid[0].length - 1; j >= 0 && j > right; j--) {
+                if(grid[i][j] == 1) {
+                    right = Math.max(right, j);
+                    break;
+                }
+            }
+        }
+
+        return (bottom - top + 1) * (right - left + 1);
     }
 }
