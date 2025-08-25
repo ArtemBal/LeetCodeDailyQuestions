a90dca9c5adc29f2cb70c83484bf2c35c4d84ac3
Artem Balan <a.s.balan96@gmail.com>
2025-08-25 11:16:37 +0300

    498 3ms using array

--- src/Problems/Q498.java
+++ src/Problems/Q498.java
@@ -1,45 +1,42 @@
 package Problems;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Q498 {
     public int[] findDiagonalOrder(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-        List<Integer> ans = new ArrayList<>(n * m);
+        int[] ans = new int[m * n];
         int i = 0;
         int j = 0;
-        ans.add(mat[0][0]);
-        while(ans.size() < n * m) {
+        int index = 0;
+        ans[index++] = mat[0][0];
+        while(index < n * m) {
             while(i + 1 < n && j - 1 >= 0) {
                 i++;
                 j--;
-                ans.add(mat[j][i]);
+                ans[index++] = mat[j][i];
             }
             if(i < n - 1) {
                 i++;
-                ans.add(mat[j][i]);
+                ans[index++] = mat[j][i];
             } else if(j < m - 1) {
                 j++;
-                ans.add(mat[j][i]);
+                ans[index++] = mat[j][i];
             }
 
             while(i - 1 >= 0 && j + 1 < m) {
                 i--;
                 j++;
-                ans.add(mat[j][i]);
+                ans[index++] = mat[j][i];
             }
             if(j < m - 1) {
                 j++;
-                ans.add(mat[j][i]);
+                ans[index++] = mat[j][i];
             } else if(i < n - 1) {
                 i++;
-                ans.add(mat[j][i]);
+                ans[index++] = mat[j][i];
             }
 
         }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        return ans;
     }
-
 }
