c4c29f14fec2f359bdc2a5445dffc95cc8adc349
Artem Balan <a.s.balan96@gmail.com>
2025-09-25 22:18:43 +0300

    120 0ms

--- src/Problems/Q120.java
+++ src/Problems/Q120.java
@@ -1,24 +1,27 @@
 package Problems;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Q120 {
+    int m;
+    Integer[][] dp;
     public int minimumTotal(List<List<Integer>> triangle) {
-        final int INF = 10000;
-        int[] curr = new int[1];
-        curr[0] = triangle.get(0).get(0);
-        for(int i = 0; i < triangle.size() - 1; i++) {
-            int[] next = new int[i + 2];
-            Arrays.fill(next, INF);
-            for(int j = 0; j < curr.length; j++) {
-                int a = triangle.get(i + 1).get(j) + curr[j];
-                next[j] = Math.min(a, next[j]);
-                int b = triangle.get(i + 1).get(j + 1) + curr[j];
-                next[j + 1] = Math.min(b, next[j + 1]);
-            }
-            curr = next;
-        }
-        return Arrays.stream(curr).min().getAsInt();
+        m = triangle.size();
+        dp = new Integer[m][m];
+        return helper(triangle, 0, 0);
     }
+
+    private int helper(List<List<Integer>> triangle, int row, int col) {
+        if(row==m){
+            return 0;
+        }
+        if(dp[row][col]!=null){
+            return dp[row][col];
+        }
+        int left = helper(triangle,row+1,col);
+        int right = helper(triangle,row+1,col+1);
+        return dp[row][col]=triangle.get(row).get(col) + Math.min(left,right);
+
+    }
+
 }
