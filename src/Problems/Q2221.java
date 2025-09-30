2990a4cf321b51191f295b360c0845df15c8b400
Artem Balan <a.s.balan96@gmail.com>
2025-09-30 19:02:51 +0300

    2221 29ms

--- src/Problems/Q2221.java
+++ src/Problems/Q2221.java
@@ -2,10 +2,14 @@ package Problems;
 
 public class Q2221 {
     public int triangularSum(int[] nums) {
+        int[] MODS = new int[20];
+        for(int i = 0; i < 20; i++) {
+            MODS[i] = i % 10;
+        }
         int n = nums.length - 1;
         while(n >= 0) {
             for(int i = 0; i < n; i++) {
-                nums[i] = (nums[i] + nums[i + 1]) % 10;
+                nums[i] = MODS[nums[i] + nums[i + 1]];
             }
             n--;
         }
