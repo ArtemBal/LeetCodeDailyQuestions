39122689d6c25ebb467bac28e0c18af843c17789
Artem Balan <a.s.balan96@gmail.com>
2025-09-30 18:58:49 +0300

    2221 42ms

---
+++ src/Problems/Q2221.java
@@ -0,0 +1,14 @@
+package Problems;
+
+public class Q2221 {
+    public int triangularSum(int[] nums) {
+        int n = nums.length - 1;
+        while(n >= 0) {
+            for(int i = 0; i < n; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            n--;
+        }
+        return nums[0];
+    }
+}
