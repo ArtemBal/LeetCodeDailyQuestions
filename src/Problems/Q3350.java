d7d83baa7f0d56416065e5d23cb9a6aa57b2d994
Artem Balan <a.s.balan96@gmail.com>
2025-10-15 18:19:34 +0300

    3350 14ms without array

--- src/Problems/Q3350.java
+++ src/Problems/Q3350.java
@@ -5,25 +5,19 @@ import java.util.List;
 public class Q3350 {
     public int maxIncreasingSubarrays(List<Integer> nums) {
         int n = nums.size();
-        int[] incr = new int[n];
-        incr[0] = 1;
+        int max = 0;
+        int prev = 0;
+        int curr = 1;
         for(int i = 1; i < n; i++) {
             if(nums.get(i) > nums.get(i - 1)) {
-                incr[i] = incr[i - 1] + 1;
+                curr++;
             } else {
-                incr[i] = 1;
+                max = Math.max(max, Math.max(Math.max(prev, curr) / 2, Math.min(prev, curr)));
+                prev = curr;
+                curr = 1;
             }
         }
-        int max = 0;
-        int prev = incr[n - 1];
-        if(prev == n) {
-            return n / 2;
-        }
-        for(int i = n - 1 - prev; i >= 0; i -= prev) {
-            int curr = incr[i];
-            max = Math.max(max, Math.max(Math.max(prev, curr) / 2, Math.min(prev, curr)));
-            prev = curr;
-        }
+        max = Math.max(max, Math.max(Math.max(prev, curr) / 2, Math.min(prev, curr)));
         return max;
     }
 }
