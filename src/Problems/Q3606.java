0c758f6b37e29633491eca0c2633e49a8d05408f
Artem Balan <a.s.balan96@gmail.com>
2025-12-13 17:59:59 +0200

    3606 25ms

---
+++ src/Problems/Q3606.java
@@ -0,0 +1,43 @@
+package Problems;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q3606 {
+    public enum Business {
+        electronics, grocery, pharmacy, restaurant
+    }
+
+    public record Coupon(String code, Business business) {
+
+        @Override
+            public String toString() {
+                return code + " (" + business + ")";
+            }
+        }
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        List<Coupon> coupons = new ArrayList<>();
+        for(int i = 0; i < code.length; i++) {
+            if(checkBusiness(businessLine[i]) && checkId(code[i]) && isActive[i]) {
+                coupons.add(new Coupon(code[i], Business.valueOf(businessLine[i])));
+            }
+        }
+        coupons.sort(Comparator.comparing(Coupon::business).thenComparing(Coupon::code));
+        return coupons.stream().map(Coupon::code).collect(Collectors.toList());
+    }
+
+    public static boolean checkBusiness(String v) {
+        for (Business b : Business.values()) {
+            if (b.name().equals(v)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkId(String s) {
+        return s.matches("\\w+");
+    }
+}
