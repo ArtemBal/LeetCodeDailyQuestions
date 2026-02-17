c47b37f64fac2f37069aba42c89816e94c128a91
Artem Balan <a.s.balan96@gmail.com>
2026-02-17 17:48:53 +0200

    401 5ms

---
+++ src/Problems/Q401.java
@@ -0,0 +1,22 @@
+package Problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q401 {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans = new ArrayList<>();
+        if(turnedOn > 8) {
+            return ans;
+        }
+        for(int h = 0; h < 12; h++) {
+            for(int m = 0; m < 60; m++) {
+                if(Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    ans.add(h + ":" + (m < 10 ? "0" : "") + m);
+                }
+            }
+        }
+        return ans;
+    }
+
+}
